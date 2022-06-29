4fe6f1cc759d7a1b74f0518e0ae76503628d23a4
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-06-29 17:50:48 +0800

    set admin/user style use will_paginate to layout

--- app/controllers/dashboard/admin/users_controller.rb
+++ app/controllers/dashboard/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Dashboard::Admin::UsersController < Dashboard::DashboardController
   def index
-    @users = User.all
+    @users = @paginate = User.all.paginate(page: params[:page], per_page: 10).order("id DESC")
   end
 end
