93fbf77026e1f6510d493d040a995b5f49890610
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-01 15:13:16 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "pages#index"
   devise_for :users, controllers: { session: "users/sessions", registrations: "users/registrations" }
-  devise_for :managers
 
   namespace :dashboard do
     namespace :admin do
