389b26cab9169197fe519fa9f55b82339a60e0d9
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-01 15:49:57 +0800

    change Products => remote_image_url to remote_cover_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_074200) do
+ActiveRecord::Schema.define(version: 2022_07_01_074813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_07_01_074200) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "cover"
-    t.string "remote_image_url"
+    t.string "remote_cover_url"
     t.integer "categories"
   end
 
