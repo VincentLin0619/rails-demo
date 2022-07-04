a08da59d970901647cdb2ad97008448f10c52b39
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-04 17:58:11 +0800

    paginate in product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = @paginate = Product.all.paginate(page: params[:page], per_page: 10).order(id: :desc)
   end
 end
