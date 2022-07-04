94e53cccb438393eb6ff085b5ba1bc208f23ad1d
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-04 17:52:39 +0800

    create fake data

---
+++ db/migrate/20220704085259_add_clolumn_to_product.rb
@@ -0,0 +1,5 @@
+class AddClolumnToProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :products, :cover, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_074813) do
+ActiveRecord::Schema.define(version: 2022_07_04_085259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,11 @@ ActiveRecord::Schema.define(version: 2022_07_01_074813) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "sku"
     t.integer "price"
+    t.integer "sku"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "cover"
-    t.string "remote_cover_url"
-    t.integer "categories"
   end
 
   create_table "users", force: :cascade do |t|
