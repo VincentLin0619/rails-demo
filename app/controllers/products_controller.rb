35ac379ceec86ce454111f4e407e80374fef5355
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-05 17:08:47 +0800

    add productController#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = @paginate = Product.all.paginate(page: params[:page], per_page: 8).order(id: :desc)
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
 end
