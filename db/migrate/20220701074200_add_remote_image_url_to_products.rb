313e8ca02cc8d8c9d8771e99151d6e65ad0f4b27
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-07-06 10:30:32 +0800

    fix migrateion

--- db/migrate/20220701074200_add_remote_image_url_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddRemoteImageUrlToProducts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :products, :remote_image_url, :string
-    add_column :products, :categories, :integer
-  end
-end
--- db/migrate/20220701074813_change_name_from_products.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameFromProducts < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :products, :remote_image_url, :remote_cover_url
-  end
-end
